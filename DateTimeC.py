878b55eb89ab9ca9cfa983ea48cc913907a9cf5d
11AZ <hazwanhaz2010@gmail.com>
2020-12-14 08:57:07 +0000

    basic finish

--- DateTimeC.py
+++ DateTimeC.py
@@ -1,21 +1,18 @@
+# client.py  
 import socket
-import sys
-import datetime
-#Create client socket
-s = socket.socket()
 
-#Server ip address and port use
-host = "192.168.0.101"
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = "192.168.0.101"                           
+
 port = 8888
 
-#Connect to server
-print(f"[+] Connecting to {host}:{port}")
-s.connect((host,port))
-print("[+] connected")
-
-#User input
-user = input("Enter <today> to know today date and time:")
-s.send(f"{user}".encode())
-
-#Close socket
+# connection to hostname on the port.
+s.connect((host, port))
+# Receive no more than 1024 bytes
+tm = s.recv(1024)
 s.close()
+
+print("The time got from the server is %s" % tm.decode('utf-8'))
