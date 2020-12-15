c470ac328d42b6025236c0ccea26cf610ba3afa1
11AZ <hazwanhaz2010@gmail.com>
2020-12-15 22:30:20 +0800

    Error in while loop

--- DateTimeC.py
+++ DateTimeC.py
@@ -1,15 +1,16 @@
-import time
 import socket
 import sys
 from tqdm import tqdm , trange
+import datetime
+import time
+from time import sleep
 # create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket()
 
-# get local machine name
+# get local server machine name
 host = "192.168.0.101"
 
 port = 8888
-
 # connection to hostname on the port.
 print(f"[+] Connecting to {host}:{port}")
 #Progress bar
@@ -20,13 +21,38 @@ for i in range(10):
 pbar.close()
 s.connect((host, port))
 print("[+] connected")
+time.sleep(1)
+#whle lopp
+while True:
+	#Front page
+	print('-----------------------WELCOME TO THE DATETIME SERVER-----------------------')
+	print('1-GO TO DATETIME SERVICE')
+	print('bye- TO EXIT THE PROGRAM')
+	a = input('Enter selection:')
+	print(a)
+	#if statement
+	if a == '1':
+		#Datetime Page
+		print('Welcome to the Date time service')
+		print('Enter <1> to check today date and time')
+		print('Enter <2> to check yesterday date')
+		print('Enter <3> to check tommorrow date')
+		print('Enter <99> to exit the program')
+		date = input('Choose what you want: ')
+		if date == '1':
+			s.send(date.encode()) #Send date to server
+			tm = s.recv(36) #Receive time from server
+			print('Today time : %s' % tm.decode('utf-8'))
+		elif date == '2':
+			s.send(date.encode())
+			tmY = s.recv(23) #Receive yesterday date
+			print('Yesterday date :%s'% tmY.decode('utf-8'))
+		elif date == '3':
+			s.send(date.encode())
+			tmT = s.recv(23) #Receive tommorow date
+			print('Tommorrow date : %s' % tmT.decode('utf-8'))
+		else:
+			print('No input from server')
+	else:
+		print('Error')
 
-#receive answer from server
-msg=s.recv(1024)
-print(msg.decode("utf-8"))
-
-# Receive no more than 1024 bytes
-tm = s.recv(1024)
-print("The time got from the server is %s" % tm.decode('utf-8'))
-
-s.close()
