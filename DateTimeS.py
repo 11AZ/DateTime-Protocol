5642d23dcdb10928e924cd0068b09275e60d19b7
11AZ <hazwanhaz2010@gmail.com>
2020-12-19 11:39:52 +0800

    Finish

--- DateTimeS.py
+++ DateTimeS.py
@@ -6,8 +6,10 @@ import tqdm
 from time import sleep
 import sys
 from datetime import timedelta
+import os
+from _thread import *
 # create a socket object
-serversocket = socket.socket()
+s = socket.socket()
 
 #Word display function
 def typewritter(msg):
@@ -16,35 +18,50 @@ def typewritter(msg):
                 sys.stdout.flush()
                 time.sleep(0.05)
 
+
+def close():
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
+        print("Server close")
+
 typewritter("[+] Socket successfully created\n")
 # get local machine name
 host = socket.gethostname()
 port = 8888
+ThreadCount = 0
 # bind to the port
-serversocket.bind((host,port))
+s.bind((host,port))
 typewritter("[+] Socket is binded to "+str(port)+"\n")
 # queue up to 5 requests
-serversocket.listen(5)
+s.listen(5)
 typewritter("[+] Socket is listening\n")
 #Waiting for connection from client
 typewritter("[+] Socket is waiting connection from client\n")
-
-while True:
-        # establish a connection
-        clientsocket,addr = serversocket.accept()
-        typewritter("[+] Connetion establish\n")
-	while True:
-                sel = clientsocket.recv(1024).decode('utf-8')
+def threaded_client(connection):
+        typewritter("[+] Connetion establish \n")
+        while True:
+                # establish a connection
+                sel = connection.recv(1024).decode('utf-8')
                 if sel == '1':
                         ctime=datetime.datetime.now().strftime('%A %d %B %Y %H:%M:%S %p')
-                        clientsocket.send(str(ctime).encode())
+                        connection.send(str(ctime).encode())
                 elif sel == '2':
                         ytime=datetime.datetime.now() - datetime.timedelta(days = 1)
-                        clientsocket.send(str(ytime.strftime('%A %d %B %Y')).encode())
+                        connection.send(str(ytime.strftime('%A %d %B %Y')).encode())
                 elif sel == '3':
                         Ttime = datetime.datetime.now() + datetime.timedelta(days = 1)
-                        clientsocket.send(str(Ttime.strftime('%A %d %B %Y')).encode())
+                        connection.send(str(Ttime.strftime('%A %d %B %Y')).encode())
                 elif sel == '0':
-                        clientsocket.close()
-        clientsocket.close()
-clientsocket.close()
+                        close()
+        connection.close()
+#Main
+while True:
+        client,addr = s.accept()
+        print('Connected to :'+addr[0]+':'+str(addr[1]))
+        start_new_thread(threaded_client,(client,))
+        ThreadCount += 1
+        print('Thread Number:' + str(ThreadCount))
+s.close()
+
+
+
